7c734be2887dbaf6d0dc0419b4241243b03a096f
Vitaliy Angolenko <v.angolenko@deepvision.team>
2021-01-31 10:54:55 +0200

    feat: vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@
 set -e
 
 function main() {
-  http \
-    \ --ignore-stdin 
-    \ --verbose POST 
-    \ "https://europe-west3-microops.cloudfunctions.net/microops"
-    \ repo="github.com/${GITHUB_REPOSITORY}"
-    \ ref=$(echo ${GITHUB_REF} | sed -e "s/refs\///g")
+  DEPLOY_URL="https://europe-west3-microops.cloudfunctions.net/microops"
+  DEPLOY_REPO="github.com/${GITHUB_REPOSITORY}"
+  DEPLOY_REF=$(echo ${GITHUB_REF} | sed -e "s/refs\///g")
+
+  http --ignore-stdin --verbose POST "${DEPLOY_URL}" repo="${DEPLOY_REPO}" ref="${DEPLOY_REF}"
 }
 
 main
