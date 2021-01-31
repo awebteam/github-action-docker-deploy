cd0ee9aa7bb692d55799d3fc0f04974fc741377a
Vitaliy Angolenko <v.angolenko@deepvision.team>
2021-01-31 10:58:37 +0200

    feat: deploy post

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 function main() {
-  DEPLOY_URL="https://europe-west3-microops.cloudfunctions.net/microops"
+  DEPLOY_URL="https://europe-west3-microops.cloudfunctions.net/microops/deploy"
   DEPLOY_REPO="github.com/${GITHUB_REPOSITORY}"
   DEPLOY_REF=$(echo ${GITHUB_REF} | sed -e "s/refs\///g")
 
